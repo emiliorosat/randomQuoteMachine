6162b59efd633ac51c3a49d50adce3e98cb67830
Emilio Rosa <42384263+Aaronsn0w@users.noreply.github.com>
2019-12-24 12:54:00 -0400

    Maquetacion lista estilo sencillo con bootstrap

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-$(document).ready(function(){
-  
-    const URL = "https://thesimpsonsquoteapi.glitch.me/quotes";
-    
-    fetchQuote()
+$(document).ready(function() {
+  const URL = "https://thesimpsonsquoteapi.glitch.me/quotes";
 
-    $('#new-quote').on('click', ()=>{
-        fetchQuote()
-    })
+  fetchQuote();
 
-    function fetchQuote(){
-        $.ajax({
-            url: URL,
-            success: (data)=>{
-                $('#text').html(data[0].quote)
-                $('#author').html(data[0].character)
-                console.log(data)
-            }
-        })
-    }
+  $("#new-quote").on("click", () => {
+    fetchQuote();
+  });
 
-})
+  function fetchQuote() {
+    $.ajax({
+      url: URL,
+      success: data => {
+        $("#text").html('<blockquote>"' + data[0].quote + '"</blockquote>');
+        $("#author").html("Author: <strong>" + data[0].character + "</strong>");
+      }
+    });
+  }
+});
